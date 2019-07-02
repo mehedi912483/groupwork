0642002cd1a380ff59c13ceb5f10b7bdf79210cd
mehedi912483 <mehedi.bpi@gmail.com>
2019-07-02 15:21:26 +0600

    added change file

--- groupWork/groupWork/MyInfo.cs
+++ groupWork/groupWork/MyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace groupWork
 {
-    public partial class Form1 : Form
+    public partial class MyInfo : Form
     {
-        public Form1()
+        public MyInfo()
         {
             InitializeComponent();
         }
